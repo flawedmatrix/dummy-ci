4e4cda53f0863a40f4477c8ef40f33be0eedb264
pivotal <pivotal@midway.sf.pivotallabs.com>
2015-08-31 12:07:02 -0700

    Narrowed down to simplest case to test pipeline

--- tasks/promote-candidate.sh
+++ tasks/promote-candidate.sh
@@ -2,12 +2,12 @@
 
 set -e -x
 
-release_version=`cat release_version_semver/number`
+# release_version=`cat release_version_semver/number`
 
 cd dummy-repo
 
-touch $release_version
-
-git add .
-
-git commit -m "New final release v $release_version"
+# touch $release_version
+# 
+# git add .
+# 
+# git commit -m "New final release v $release_version"
